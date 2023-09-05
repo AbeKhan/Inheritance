39ba50ebb33f6939660fad3eaa8a0874a1666a7d
AbeKhan <128449118+AbeKhan@users.noreply.github.com>
2023-09-05 14:56:43 -0500

    Contrustor for GradStudent, also getters & setters

--- src/App.java
+++ src/App.java
@@ -3,7 +3,14 @@ public class App {
     {
         Student s = new Student();
         Student d = new Student("Abraham", "Khan", 3.0, "1048135");
+        GradStudent grad = new GradStudent();
+        GradStudent coin = new GradStudent("Abraham", "Khan", 3.0, "1048135", "animal", "9-6");
         System.out.println(s);
         System.out.println(d);
+        System.out.println(grad);
+        System.out.println(coin);
+        System.out.println(grad.getOffice());
+        System.out.println(grad.getID());
+        
     }
 }
--- src/GradStudent.java
+++ src/GradStudent.java
@@ -2,4 +2,49 @@ public class GradStudent extends Student
 {
     String offices;
     String officeHours;
+
+    public GradStudent()
+    {
+        super();
+        super.ID = "GR-" + ID;
+        offices = "N/A";
+        officeHours = "N/A";
+    }
+
+    public GradStudent(String FN, String LN, Double GPA, String ID, String offices, String officeHours)
+    {
+        super(FN, LN, GPA, ID);
+        /*super.firstName = FN;
+        super.lastName = LN;
+        super.GPA = GPA;
+        super.ID = "GR-" + ID;*/
+        this.offices = offices;
+        this.officeHours = officeHours;
+    } 
+    @Override
+    public String setID(String id)
+    {
+        //this.ID = super.setID(id);
+        String holder = ID;
+        this.ID = "GR-" + super.setID(id);
+        return "ID has changed from " + holder + " to " + ID;
+    }
+
+    public String getOffice()
+    {
+        return "Office:" + offices;
+    }
+
+    public String setOffice(String office)
+    {
+        String holder = offices;
+        this.offices = office;
+        return "Office has changed from " + holder + " to " + offices;
+    }
+
+    @Override
+    public String toString()
+    {
+        return super.toString() + " Office: " + offices + " Office Hours:" + officeHours;
+    }
 }
